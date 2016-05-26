498076f4dc81d5901ad9ff131c0e3849c11bd89e
daniel sim <daniel_sim@data.gov.sg>
2016-05-26 14:33:29 +0800

    late -> delayed

--- bundle.js
+++ bundle.js
@@ -30464,7 +30464,7 @@
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
-	exports.default = [['Bus Cancellation', "(DO NOT REPLY) Attention: The service for today has been cancelled due to unforeseen circumstances. " + "Please make alternative transport arrangements. Today's fare will be refunded and we sincerely apologies for the inconvenience caused to all our commuters."], ['Bus is going to be more than 15 mins late', '(DO NOT REPLY) Attention: Service for today will be late. The bus is still on its way to your stop. Sorry for the inconvenience.'], ['Driver App is unavailable', '(DO NOT REPLY) Attention: The location tracking on the app is not working today. The bus is still on its way to your stop. We sincerely apologies for the inconvenience caused to all our commuters.']];
+	exports.default = [['Bus Cancellation', "(DO NOT REPLY) Attention: The service for today has been cancelled due to unforeseen circumstances. " + "Please make alternative transport arrangements. Today's fare will be refunded and we sincerely apologies for the inconvenience caused to all our commuters."], ['Bus is going to be more than 15 mins late', '(DO NOT REPLY) Attention: Service for today will be delayed. The bus is still on its way to your stop. Sorry for the inconvenience.'], ['Driver App is unavailable', '(DO NOT REPLY) Attention: The location tracking on the app is not working today. The bus is still on its way to your stop. We sincerely apologies for the inconvenience caused to all our commuters.']];
 
 /***/ },
 /* 33 */
