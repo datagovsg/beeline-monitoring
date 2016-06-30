564dc9bf83f3a0a8e86ee2ec1cae976f800c5964
daniel sim <daniel_sim@data.gov.sg>
2016-06-30 10:11:48 +0800

    typo apologies -> apologise

--- client/message-templates.js
+++ client/message-templates.js
@@ -2,7 +2,7 @@ export default [
     [
     'Bus Cancellation',
     "(DO NOT REPLY) Attention: The service for today has been cancelled due to unforeseen circumstances. " + 
-       "Please make alternative transport arrangements. Today's fare will be refunded and we sincerely apologies for the inconvenience caused to all our commuters."
+       "Please make alternative transport arrangements. Today's fare will be refunded and we sincerely apologise for the inconvenience caused to all our commuters."
     ],
     [
     'Bus is going to be more than 15 mins late',
@@ -10,6 +10,6 @@ export default [
     ],
     [
     'Driver App is unavailable',
-    '(DO NOT REPLY) Attention: The location tracking on the app is not working today. The bus is still on its way to your stop. We sincerely apologies for the inconvenience caused to all our commuters.'
+    '(DO NOT REPLY) Attention: The location tracking on the app is not working today. The bus is still on its way to your stop. We sincerely apologise for the inconvenience caused to all our commuters.'
     ],
 ]
