50d2e4c4a7aa2230cc8499bf80855be5f254a21c
daniel sim <daniel_sim@data.gov.sg>
2016-11-25 09:43:56 +0800

    announcement update (show current announcement)

--- client/route-announcement-form.vue
+++ client/route-announcement-form.vue
@@ -1,25 +1,81 @@
 <template>
+  <div>
+    <b>Current Message:</b>
+    <i v-if="currentMessage === null">Loading...</i>
+    <i v-if="currentMessage === ''">(none)</i>
+    <span class="message-box" v-if="currentMessage">
+      {{currentMessage}}
+    </span>
+  </div>
   <form @submit="updateRouteAnnouncements">
-      <textarea v-model="message"
-          style="display: block; width: 100%; height: 100px"
-          name="message"></textarea>
+    <div>
+      <label>
+        New message (leave blank to clear the message)<br/>
+        <textarea v-model="message"
+            style="display: block; width: 100%; height: 100px"
+            name="message"></textarea>
+      </label>
+    </div>
+    <div>
       <button class="message-button" type="submit">Submit</button>
+    </div>
   </form>
 </template>
+
+<style scoped>
+.message-box {
+  border: solid 1px #888;
+  background-color: #FFD;
+  padding: 0.5em;
+  font-size: 120%;
+  display: inline-block;
+}
+label {
+  display: block;
+}
+</style>
 
 <script>
 const {authAjax} = require('./login');
 
 export default {
   props: ['tripId'],
-  
+
   data() {
+    this.requery();
     return {
       message: '',
+      currentMessage: null,
+    }
+  },
+
+  watch: {
+    tripId() {
+      this.requery();
     }
   },
 
   methods: {
+    requery() {
+      if (this.tripId) {
+        authAjax(`/trips/${this.tripId}/statuses`, {
+          method: 'GET',
+        })
+        .then(r => r.json())
+        .then((result) => {
+          // Get and show the one with the latest time
+          if (result.length === 0)
+            this.currentMessage = '';
+          else
+            this.currentMessage = _.maxBy(result, 'createdAt').message;
+        })
+        .then(() => {
+
+        })
+      } else {
+        this.currentMessage = null;
+      }
+    },
     updateRouteAnnouncements($event) {
       event.preventDefault()
 
@@ -30,8 +86,9 @@ export default {
             message: this.message,
           }
       })
-      .then(() => {
-          alert("Announcement updated!");
+      .then(r => r.json())
+      .then((result) => {
+          this.requery();
       })
       .then(null, (err) => {
           alert("There was an error updating the announcements");
