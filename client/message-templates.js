6b4ace46e5f600cc16f1adae2dc2e4ed79c35e85
daniel <daniel_sim@data.gov.sg>
2016-12-12 14:32:26 +0800

    add message template for cancelled trip

--- client/message-templates.js
+++ client/message-templates.js
@@ -1,10 +1,14 @@
 export default [
     [
-    'Bus is going to be more than 15 mins late',
-    '(DO NOT REPLY) Attention: Service for today will be delayed. The bus is still on its way to your stop. Sorry for the inconvenience.'
+      'Bus is going to be more than 15 mins late',
+      '(DO NOT REPLY) Attention: Service for today will be delayed. The bus is still on its way to your stop. Sorry for the inconvenience.'
     ],
     [
-    'Driver App is unavailable',
-    '(DO NOT REPLY) Attention: The location tracking on the app is not working today. The bus is still on its way to your stop. We sincerely apologise for the inconvenience caused to all our commuters.'
+      'Driver App is unavailable',
+      '(DO NOT REPLY) Attention: The location tracking on the app is not working today. The bus is still on its way to your stop. We sincerely apologise for the inconvenience caused to all our commuters.'
     ],
+    [
+      'Cancelled trip',
+      '(DO NOT REPLY) Attention: The service for today has been cancelled due to unforeseen circumstances. Please make alternative transport arrangements. Today\'s fare will be refunded and we sincerely apologise for the inconvenience caused to all our commuters.'
+    ]
 ]
