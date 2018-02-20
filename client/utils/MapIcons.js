5248b69f901248d1e0ce9514dc81eeb002289a31
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-02-20 16:51:19 +0800

    move map icons into a separate component
    The component will wait for `google` API to be loaded,
    so that there will be no `google is undefined` error messages

---
+++ client/utils/MapIcons.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import {loaded} from 'vue2-google-maps'
+import leftPad from 'left-pad'
+
+export default new Vue({
+  data () {
+    return {
+      startPoint: null,
+      endPoint: null,
+      mapLoaded: false,
+    }
+  },
+
+  created () {
+    loaded.then(() => {
+      _.assign(this, {
+        mapLoaded: true,
+        startPoint: {
+          url: 'img/routeStartMarker.png',
+          size: new google.maps.Size(50, 40),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(25, 40)
+        },
+        endPoint: {
+          url: 'img/routeEndMarker.png',
+          size: new google.maps.Size(50, 40),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(25, 40)
+        }
+      })
+    })
+  },
+
+  methods: {
+    stopIcon (stop, index) {
+      return this.mapLoaded && {
+        url: 'img/stop' +
+          (stop.canBoard ? 'Board' : 'Alight') +
+          leftPad(index + 1, 3, '0') + '.png',
+        size: new google.maps.Size(100, 100),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(20, 20),
+        scaledSize: new google.maps.Size(40, 40),
+      }
+    },
+  }
+})
