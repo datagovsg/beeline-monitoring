bdc96960184d6df3280b93c13455538f8bbef523
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-03-27 14:52:24 +0800

    more message templates (email on 26 Mar)

--- client/message-templates.js
+++ client/message-templates.js
@@ -8,6 +8,22 @@ export default [
       '(DO NOT REPLY) Attention: Service for today will be delayed for 15 mins. The bus is still on its way to your stop. Sorry for the inconvenience.'
     ],
     [
+      'Bus Breakdown',
+      '[Bus Breakdown] Please make alternative transportation arrangement. For more details, please contact XXXX @ 12345678'
+    ],
+    [
+      'Road Accident',
+      '[Bus Involved In An Accident] Please make alternative transportation arrangement. For more details, please contact XXXX @ 12345678'
+    ],
+    [
+      'Bus Delay/Traffic Jam',
+      '[Bus Delay] The scheduled bus will be delayed for X mins due to bad traffic condition.'
+    ],
+    [
+      'Bus Replacement',
+      '[Bus Replacement] The scheduled bus has been replaced with XXXX.  For more details, please contact XXXX @ 12345678'
+    ],
+    [
       'Bus is going to be more than 15 mins late',
       '(DO NOT REPLY) Attention: Service for today will be delayed. The bus is still on its way to your stop. Sorry for the inconvenience.'
     ],
