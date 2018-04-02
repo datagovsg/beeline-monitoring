7d94ded86c76d4b2c3a96d5f3eb54d3313b18fc7
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-04-02 11:23:30 +0800

    move messages from message-templates to announcement-templates

--- client/announcement-templates.js
+++ client/announcement-templates.js
@@ -1,11 +1,22 @@
 export default [
-    [
-      'Bus Replacement',
-      'Bus no, <license plate number>, has replaced one of our regular buses. \nPlease look out for a different bus for today.  For enquiries, call <Operator Name> at <Contact Number>.'
-    ],
-    [
-      'No Bus Replacement',
-      'The bus at <route time> is not running for today due to <reason e.g. bus breakdown>. \nPlease look out for a different bus for today. For enquiries, call <Operator Name> at <Contact Number>.'
-    ],
-
+  [
+    'Bus Breakdown',
+    '[Bus Breakdown] Please make alternative transportation arrangement. For more details, please contact XXXX @ 12345678'
+  ],
+  [
+    'Road Accident',
+    '[Bus Involved In An Accident] Please make alternative transportation arrangement. For more details, please contact XXXX @ 12345678'
+  ],
+  [
+    'Bus Delay/Traffic Jam',
+    '[Bus Delay] The scheduled bus will be delayed for X mins due to bad traffic condition.'
+  ],
+  [
+    'Bus Replacement',
+    '[Bus Replacement] The scheduled bus has been replaced with XXXX.  For more details, please contact XXXX @ 12345678'
+  ],
+  [
+    'No Bus Replacement',
+    'The bus at <route time> is not running for today due to <reason e.g. bus breakdown>. \nPlease look out for a different bus for today. For enquiries, call <Operator Name> at <Contact Number>.'
+  ],
 ]
